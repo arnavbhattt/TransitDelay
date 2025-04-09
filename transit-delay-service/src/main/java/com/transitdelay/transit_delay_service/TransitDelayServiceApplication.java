78edbab5a35b0f130ca955afba7a2ceb9195d9ff
arnavbhattt <arnxv23@gmail.com>
2025-04-08 23:11:37 -0400

    Init Spring Project

---
+++ transit-delay-service/src/main/java/com/transitdelay/transit_delay_service/TransitDelayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.transitdelay.transit_delay_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitDelayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitDelayServiceApplication.class, args);
+	}
+
+}
---
+++ transit-delay-service/src/test/java/com/transitdelay/transit_delay_service/TransitDelayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transitdelay.transit_delay_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitDelayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
